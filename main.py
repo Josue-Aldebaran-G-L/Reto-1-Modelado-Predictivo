304b1ad37e33297d7cb4595a75c26cbbbdcbe173
Josue-Aldebaran-G-L <gjosuealdebaran@gmail.com>
2026-03-22 20:21:20 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import sklearn as skl
 import pandas as pd
+import pickle as pkl
 
 def sistema_inteligente(
     bill_length_mm,
     bill_depth_mm,
     flipper_length_mm,
-    body_mass_g):
+    body_mass_g
+    ):
     """
     Clasifica un pingüino basándose en reglas diseñadas por un humano.
 
@@ -23,6 +24,7 @@ def sistema_inteligente(
     Retorna:
     --------
     str : 'Adelie', 'Chinstrap', o 'Gentoo'
+      Raza del pingüino
     """
 
     if flipper_length_mm > 200:
@@ -40,6 +42,20 @@ def sistema_inteligente(
             return "Adelie"
 
 def clasificador_humano(data):
+    """
+    Basado en el sistema inteligente, recibe la información general
+    y predice la especie de cada pingüino.
+
+    Parámetros:
+    -----------
+    data : pd.DataFrame
+      Datos a predecir
+
+    Retorna:
+    --------
+    predicciones_humano : list
+      Predicciones por pingüino.
+    """
     predicciones_humano = []
 
     for idx, row in data.iterrows():
@@ -54,32 +70,44 @@ def clasificador_humano(data):
 
     return predicciones_humano
 
-def comparador_humano_vs_maquina(X_test, y_test, modelo_maquina):
-    y_pred_maquina = modelo_maquina.predict(X_test)
-    acurracy_maquina = skl.metrics.acurracy_score(y_test, y_pred_maquina)
+def comparador_humano_vs_maquina(data, modelo_maquina, modelo_humano):
+    """
+    Devuelve un csv y lo guarda en memoria.
 
-    y_pred_humano = clasificador_humano(X_test)
-    acurracy_humano = skl.metrics.accuracy_score(y_test, y_pred_humano)
+    Parámetros:
+    -----------
+    data : pd.DataFrame
+      Datos a predecir
+    modelo_maquina : sklearn.tree.DecisionTreeClassifier
+      Modelo de árbol de decisión entrenado.
+    modelo_humano : function
+      Función que predice la especie de cada pingüino.
 
-    return (acurracy_maquina, acurracy_humano)
+    Retorna:
+    --------
+    results : pd.DataFrame
+      Predicciones por pingüino.
+    """
+
+    y_pred_maquina = modelo_maquina.predict(data)
+    y_pred_humano = modelo_humano(data)
+    
+    results = pd.DataFrame({
+        'y_pred_maquina': y_pred_maquina,
+        'y_pred_humano': y_pred_humano
+    })
+
+    results.to_csv('results.csv', index=False)
+    return results
 
 
-path = ""
+path = "data.csv"
 data = pd.read_csv(path)
 
 X = data[['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g']]
-y = data['species']
 
-X_train, X_test, y_train, y_test = skl.model_selection.train_test_split(
-    X,
-    y,
-    test_size=0.2
-    random_state=42
-    stratify=y
-
-)
-
-modelo_maquina = skl.tree.DecisionTreeClassifier(random_state=42)
-modelo_maquina.fit(X_train, y_train)
-
-comparador_humano_vs_maquina(X_test, y_test, modelo_maquina)
+modelo_maquina_path = "modelo_ml.pkl"
+with open("modelo_ml.pkl", 'rb') as f:
+    modelo_maquina = pkl.load(f)
+    
+print(comparador_humano_vs_maquina(X, modelo_maquina, clasificador_humano))
